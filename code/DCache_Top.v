7b4155d04d971d3865014d78fd87ca9ac5371804
adrien1018 <adrien1018@users.noreply.github.com>
2019-12-29 19:59:15 +0800

    Revise code

--- code/DCache_Top.v
+++ code/DCache_Top.v
@@ -24,146 +24,139 @@ module DCache_Top(
   output                p1_stall_o
 );
 
-wire [`DM_BYTE_UNIT-1:0]    cache_sram_index;
-wire                        cache_sram_enable;
-wire [`L1_TAG_MEM_SIZE-1:0] cache_sram_tag;
-wire [`DM_UNIT_MASK:0]      cache_sram_data;
-wire                        cache_sram_write;
-wire [`L1_TAG_MEM_SIZE-1:0] sram_cache_tag;
-wire [`DM_UNIT_MASK:0]      sram_cache_data;
-wire                        sram_valid;
-wire                        sram_dirty;
+  wire [`DM_BYTE_UNIT-1:0]    cache_sram_index;
+  wire                        cache_sram_enable;
+  wire [`L1_TAG_MEM_SIZE-1:0] cache_sram_tag;
+  wire [`DM_UNIT_MASK:0]      cache_sram_data;
+  wire                        cache_sram_write;
+  wire [`L1_TAG_MEM_SIZE-1:0] sram_cache_tag;
+  wire [`DM_UNIT_MASK:0]      sram_cache_data;
+  wire                        sram_valid;
+  wire                        sram_dirty;
 
-// controller
-parameter STATE_IDLE       = 2'h0,
-          STATE_READMISS   = 2'h1,
-          STATE_READMISSOK = 2'h2,
-          STATE_WRITEBACK  = 2'h3;
-reg [1:0] state;
-reg       mem_enable;
-reg       mem_write;
-reg       cache_we;
-wire      cache_dirty;
-reg       write_back;
+  // controller
+  parameter STATE_IDLE       = 2'h0,
+            STATE_READMISS   = 2'h1,
+            STATE_READMISSOK = 2'h2,
+            STATE_WRITEBACK  = 2'h3;
+  reg [1:0] state;
+  reg       mem_enable;
+  reg       mem_write;
+  reg       cache_we;
+  wire      cache_dirty;
+  reg       write_back;
 
-// regs & wires
-wire    [`DM_BYTE_UNIT-1:0]  p1_offset;
-wire    [`L1_INDEX_SIZE-1:0] p1_index;
-wire    [`L1_TAG_SIZE-1:0]   p1_tag;
-wire    [`DM_UNIT_MASK:0]    r_hit_data;
-wire    [`L1_TAG_SIZE-1:0]   sram_tag;
-wire                         hit;
-reg     [`DM_UNIT_MASK:0]    w_hit_data;
-wire                         write_hit;
-wire                         p1_req;
-reg     [`REG_LEN-1:0]       p1_data;
+  // regs & wires
+  wire [`DM_BYTE_UNIT-1:0]  p1_offset;
+  wire [`L1_INDEX_SIZE-1:0] p1_index;
+  wire [`L1_TAG_SIZE-1:0]   p1_tag;
+  wire [`DM_UNIT_MASK:0]    r_hit_data;
+  wire [`L1_TAG_SIZE-1:0]   sram_tag;
+  wire                      hit;
+  reg  [`DM_UNIT_MASK:0]    w_hit_data;
+  wire                      write_hit;
+  wire                      p1_req;
+  reg  [`REG_LEN-1:0]       p1_data;
 
-// project1 interface
-assign    p1_req     = p1_MemRead_i | p1_MemWrite_i;
-assign    p1_offset  = p1_addr_i[`DM_BYTE_UNIT-1:0];
-assign    p1_index   = p1_addr_i[`L1_SIZE-1:`DM_BYTE_UNIT];
-assign    p1_tag     = p1_addr_i[`REG_LEN-1:`L1_SIZE];
-assign    p1_stall_o = ~hit & p1_req;
-//assign    p1_data_o  = r_hit_data >> (32 * p1_offset);
-assign    p1_data_o  = p1_data;
+  // project1 interface
+  assign p1_req     = p1_MemRead_i | p1_MemWrite_i;
+  assign p1_offset  = p1_addr_i[`DM_BYTE_UNIT-1:0];
+  assign p1_index   = p1_addr_i[`L1_SIZE-1:`DM_BYTE_UNIT];
+  assign p1_tag     = p1_addr_i[`REG_LEN-1:`L1_SIZE];
+  assign p1_stall_o = ~hit & p1_req;
+  assign p1_data_o  = p1_data;
 
-// SRAM interface
-assign    sram_valid = sram_cache_tag[`L1_TAG_SIZE+1];
-assign    sram_dirty = sram_cache_tag[`L1_TAG_SIZE];
-assign    sram_tag   = sram_cache_tag[`L1_TAG_SIZE-1:0];
-assign    cache_sram_index  = p1_index;
-assign    cache_sram_enable = p1_req;
-assign    cache_sram_write  = cache_we | write_hit;
-assign    cache_sram_tag    = {1'b1, cache_dirty, p1_tag};
-assign    cache_sram_data   = hit ? w_hit_data : mem_data_i;
+  // SRAM interface
+  assign sram_valid = sram_cache_tag[`L1_TAG_SIZE+1];
+  assign sram_dirty = sram_cache_tag[`L1_TAG_SIZE];
+  assign sram_tag   = sram_cache_tag[`L1_TAG_SIZE-1:0];
+  assign cache_sram_index  = p1_index;
+  assign cache_sram_enable = p1_req;
+  assign cache_sram_write  = cache_we | write_hit;
+  assign cache_sram_tag    = {1'b1, cache_dirty, p1_tag};
+  assign cache_sram_data   = hit ? w_hit_data : mem_data_i;
 
-// memory interface
-assign    mem_enable_o = mem_enable;
-assign    mem_addr_o   = write_back ? {sram_tag, p1_index, 5'b0} : {p1_tag, p1_index, 5'b0};
-assign    mem_data_o   = sram_cache_data;
-assign    mem_write_o  = mem_write;
+  // memory interface
+  assign mem_enable_o = mem_enable;
+  assign mem_addr_o   = write_back ? {sram_tag, p1_index, 5'b0} : {p1_tag, p1_index, 5'b0};
+  assign mem_data_o   = sram_cache_data;
+  assign mem_write_o  = mem_write;
 
-assign    write_hit    = hit & p1_MemWrite_i;
-assign    cache_dirty  = write_hit;
+  assign write_hit    = hit & p1_MemWrite_i;
+  assign cache_dirty  = write_hit;
 
-   // tag comparator
-   assign hit = sram_valid && sram_tag == p1_tag;
-   assign r_hit_data = sram_cache_data;
+    // tag comparator
+  assign hit = sram_valid && sram_tag == p1_tag;
+  assign r_hit_data = sram_cache_data;
 
-   // read data :  256-bit to 32-bit
-   always @(p1_offset or r_hit_data) begin
-     p1_data <= r_hit_data >> (8 * p1_offset);
-   end
+  // read data :  256-bit to 32-bit
+  always @(p1_offset or r_hit_data) begin
+    p1_data <= r_hit_data >> (8 * p1_offset);
+  end
 
-   // write data :  32-bit to 256-bit
-   always @(p1_offset or r_hit_data or p1_data_i) begin
-      w_hit_data <= (r_hit_data & ~(256'hffffffff << (8 * p1_offset))) |
-          {224'b0, p1_data_i} << (8 * p1_offset);
-   end
+  // write data :  32-bit to 256-bit
+  always @(p1_offset or r_hit_data or p1_data_i) begin
+    w_hit_data <= (r_hit_data & ~(256'hffffffff << (8 * p1_offset))) |
+        {224'b0, p1_data_i} << (8 * p1_offset);
+  end
 
-   // controller
-   always@(posedge clk_i or negedge rst_i) begin
-      if(~rst_i) begin
-         state      <= STATE_IDLE;
-         mem_enable <= 1'b0;
-         mem_write  <= 1'b0;
-         cache_we   <= 1'b0;
-         write_back <= 1'b0;
-      end
-      else begin
-         case(state)
-           STATE_IDLE: begin
-              if(p1_req && !hit) begin
-                mem_enable <= 1'b1;
-                if(sram_dirty) begin
-                  // write back if dirty
-                  write_back <= 1'b1;
-                  mem_write <= 1'b1;
-                  state <= STATE_WRITEBACK;
-                end
-                else begin
-                  // write allocate
-                  write_back <= 1'b0;
-                  mem_write <= 1'b0;
-                  state <= STATE_READMISS;
-                end
-              end
-              else begin
-                 state <= STATE_IDLE;
-              end
-           end
-           STATE_READMISS: begin
-              if(mem_ack_i) begin
-                 cache_we <= 1'b1;
-                 mem_enable <= 1'b0;
-                 state <= STATE_READMISSOK;
-              end
-           end
-           STATE_READMISSOK: begin
-              cache_we <= 1'b0;
-              state <= STATE_IDLE;
-           end
-           STATE_WRITEBACK: begin
-              if(mem_ack_i) begin
-                 mem_enable <= 1'b1;
-                 write_back <= 1'b0;
-                 mem_write <= 1'b0;
-                 state <= STATE_READMISS;
-              end
-           end
-         endcase
-      end
-   end
+  // controller
+  always @(posedge clk_i or negedge rst_i) begin
+    if (~rst_i) begin
+      state      <= STATE_IDLE;
+      mem_enable <= 1'b0;
+      mem_write  <= 1'b0;
+      cache_we   <= 1'b0;
+      write_back <= 1'b0;
+    end else begin
+      case (state)
+        STATE_IDLE: begin
+          if (p1_req && !hit) begin
+            mem_enable <= 1'b1;
+            if(sram_dirty) begin
+              // write back if dirty
+              write_back <= 1'b1;
+              mem_write <= 1'b1;
+              state <= STATE_WRITEBACK;
+            end else begin
+              // write allocate
+              write_back <= 1'b0;
+              mem_write <= 1'b0;
+              state <= STATE_READMISS;
+            end
+          end
+        end
+        STATE_READMISS: begin
+          if (mem_ack_i) begin
+            cache_we <= 1'b1;
+            mem_enable <= 1'b0;
+            state <= STATE_READMISSOK;
+          end
+        end
+        STATE_READMISSOK: begin
+          cache_we <= 1'b0;
+          state <= STATE_IDLE;
+        end
+        STATE_WRITEBACK: begin
+          if (mem_ack_i) begin
+            mem_enable <= 1'b1;
+            write_back <= 1'b0;
+            mem_write <= 1'b0;
+            state <= STATE_READMISS;
+          end
+        end
+      endcase
+    end
+  end
 
-DCache_SRAM dcache_sram(
-  .clk_i    (clk_i),
-  .addr_i   (cache_sram_index),
-  .data_i   (cache_sram_data),
-  .tag_i    (cache_sram_tag),
-  .enable_i (cache_sram_enable),
-  .write_i  (cache_sram_write),
-  .data_o   (sram_cache_data),
-  .tag_o    (sram_cache_tag)
-);
-
+  DCache_SRAM dcache_sram(
+    .clk_i    (clk_i),
+    .addr_i   (cache_sram_index),
+    .data_i   (cache_sram_data),
+    .tag_i    (cache_sram_tag),
+    .enable_i (cache_sram_enable),
+    .write_i  (cache_sram_write),
+    .data_o   (sram_cache_data),
+    .tag_o    (sram_cache_tag)
+  );
 endmodule
--- code/Data_Memory.v
+++ code/Data_Memory.v
@@ -18,105 +18,40 @@ module Data_Memory (
   reg  [`DM_UNIT_MASK:0]          data;
   wire [`REG_LEN-`DM_BYTE_UNIT:0] addr;
 
+  // simulating
   parameter STATE_IDLE = 1'h0,
             STATE_WAIT = 1'h1;
   reg [1:0] state;
+  reg       write;
 
   assign ack_o  = (state == STATE_WAIT) && (count == `LATENCY-1);
   assign addr   = addr_i[`REG_LEN-1:`DM_BYTE_UNIT];
   assign data_o = data;
 
-  always @(posedge clk_i or negedge rst_i) begin
-    if (~rst_i) begin
+  always @(negedge rst_i or posedge clk_i) begin
+    if (~rst_i)
       state <= STATE_IDLE;
-      count <= 4'd0;
-    end
     else begin
       case (state)
         STATE_IDLE: begin
-          if(enable_i) begin
+          if (enable_i) begin
             state <= STATE_WAIT;
-            count <= count + 1;
+            count <= 1;
           end
         end
         STATE_WAIT: begin
-          if(count == `LATENCY-1) begin
+          if (count == `LATENCY-1) begin
             state <= STATE_IDLE;
-            count <= 0;
-          end
-          else begin
+            if (write_i) begin
+              memory[addr] <= data_i;
+            end else begin
+              data <= memory[addr];
+            end
+          end else begin
             count <= count + 1;
           end
         end
       endcase
     end
   end
-
-  always@(posedge clk_i) begin
-    if (ack_o) begin
-      if (write_i) begin
-        memory[addr] <= data_i;
-        data <= data_i;
-      end
-      else begin
-        data = memory[addr];
-      end
-    end
-  end
 endmodule
-
-/*
-
-`define DM_BITS 10 // 4 KiB
-`define DM_MASK ((1<<`DM_BITS)-1)
-
-module Data_Memory (
-  input         clk,
-  input  [31:0] addr,
-  input  [31:0] data,
-  input  [1:0]  width, // ins[13:12] (00: 8-bit, 01: 16-bit, 10: 32-bit)
-  input         memwrite,
-  input         sign_extend,
-  output [31:0] result
-);
-
-  reg  [31:0] memory[0:`DM_MASK];
-
-  wire [31:0] full_result;
-  wire [`DM_BITS-1:0] entry = addr[31:2] & `DM_MASK;
-
-  // Written for simplicity. This can result in out-of-bounds access, so some
-  //   of the bits in `full_result` may be invalid even if the access is valid.
-  //   However, `result` will always be valid.
-  assign full_result =
-      addr[1:0] == 2'b00 ? memory[entry] :
-      addr[1:0] == 2'b01 ? {memory[(entry+1)&`DM_MASK][7:0], memory[entry][31:8]} :
-      addr[1:0] == 2'b10 ? {memory[(entry+1)&`DM_MASK][15:0], memory[entry][31:16]} :
-                           {memory[(entry+1)&`DM_MASK][23:0], memory[entry][31:24]};
-  assign result =
-    width == 2'b10 ? full_result :
-    width == 2'b01 ? {{16{sign_extend & full_result[15]}}, full_result[15:0]} :
-                     {{24{sign_extend & full_result[7]}},  full_result[7:0]};
-
-  always @(posedge clk) begin
-    if (memwrite) begin
-      case (width)
-        2'b11:; // nothing
-        2'b10:
-          case (addr[1:0])
-            2'b00: memory[entry] <= data;
-            2'b01: {memory[(entry+1)&`DM_MASK][7:0], memory[entry][31:8]} <= data;
-            2'b10: {memory[(entry+1)&`DM_MASK][15:0], memory[entry][31:16]} <= data;
-            2'b11: {memory[(entry+1)&`DM_MASK][23:0], memory[entry][31:24]} <= data;
-          endcase
-        2'b01:
-          if (addr[1:0] != 2'b11)
-            memory[entry][addr[1:0]*8+:16] <= data[15:0];
-          else
-            {memory[(entry+1)&`DM_MASK][7:0], memory[entry][31:24]} <= data[15:0];
-        2'b00:
-          memory[entry][addr[1:0]*8+:8] <= data[7:0];
-      endcase
-    end
-  end
-endmodule*/
